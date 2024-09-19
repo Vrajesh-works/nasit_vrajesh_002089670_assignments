8a64fa1ec7b1e085fd2316a2c5440186bb8d117a
Vrajesh-works <vnasit03@gmail.com>
2024-09-19 17:41:27 -0400

    Assignment1

---
+++ Assignment1/src/model/Product.java
@@ -0,0 +1,167 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Vrajesh
+ */
+public class Product {
+    String FirstName;
+    String LastName;
+    String Email;
+    String Phone;
+    String Age;
+    String PssportNumber;
+    String Nationality;
+    String DriversLicenseNumber;
+    String DriversLicenseState;
+    String SocialSecurityNumber;
+    String AddressLine1;
+    String AddressLine2;
+    String City;
+    String State;
+    String ZIP;
+    String TelephoneNumber;
+    String FaxNumber;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getPssportNumber() {
+        return PssportNumber;
+    }
+
+    public void setPssportNumber(String PssportNumber) {
+        this.PssportNumber = PssportNumber;
+    }
+
+    public String getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(String Nationality) {
+        this.Nationality = Nationality;
+    }
+
+    public String getDriversLicenseNumber() {
+        return DriversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String DriversLicenseNumber) {
+        this.DriversLicenseNumber = DriversLicenseNumber;
+    }
+
+    public String getDriversLicenseState() {
+        return DriversLicenseState;
+    }
+
+    public void setDriversLicenseState(String DriversLicenseState) {
+        this.DriversLicenseState = DriversLicenseState;
+    }
+
+    public String getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getAddressLine1() {
+        return AddressLine1;
+    }
+
+    public void setAddressLine1(String AddressLine1) {
+        this.AddressLine1 = AddressLine1;
+    }
+
+    public String getAddressLine2() {
+        return AddressLine2;
+    }
+
+    public void setAddressLine2(String AddressLine2) {
+        this.AddressLine2 = AddressLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getZIP() {
+        return ZIP;
+    }
+
+    public void setZIP(String ZIP) {
+        this.ZIP = ZIP;
+    }
+
+    public String getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(String TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(String FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+    
+    
+}

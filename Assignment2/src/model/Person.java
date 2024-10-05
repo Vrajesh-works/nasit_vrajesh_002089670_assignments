46d74a92ad6f4bb54c2a8b161dcda649ebac7501
Vrajesh-works <vnasit03@gmail.com>
2024-10-05 01:46:37 -0400

    commit

---
+++ Assignment2/src/model/Person.java
@@ -0,0 +1,162 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Vrajesh
+ */
+public class Person {
+    String FirstName;
+    String LastName;
+    String Age;
+    String PssportNumber;
+    
+    String StreetAddress1;
+    String UnitNumber1;
+    String City1;
+    String State1;
+    String ZipCode1;
+    String Phone1;
+    
+    String StreetAddress2;
+    String UnitNumber2;
+    String City2;
+    String State2;
+    String ZipCode2;
+    String Phone2;
+
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public String getPssportNumber() {
+        return PssportNumber;
+    }
+
+    public void setPssportNumber(String PssportNumber) {
+        this.PssportNumber = PssportNumber;
+    }
+
+    public String getStreetAddress1() {
+        return StreetAddress1;
+    }
+
+    public void setStreetAddress1(String StreetAddress1) {
+        this.StreetAddress1 = StreetAddress1;
+    }
+
+    public String getUnitNumber1() {
+        return UnitNumber1;
+    }
+
+    public void setUnitNumber1(String UnitNumber1) {
+        this.UnitNumber1 = UnitNumber1;
+    }
+
+    public String getCity1() {
+        return City1;
+    }
+
+    public void setCity1(String City1) {
+        this.City1 = City1;
+    }
+
+    public String getState1() {
+        return State1;
+    }
+
+    public void setState1(String State1) {
+        this.State1 = State1;
+    }
+
+    public String getZipCode1() {
+        return ZipCode1;
+    }
+
+    public void setZipCode1(String ZipCode1) {
+        this.ZipCode1 = ZipCode1;
+    }
+
+    public String getPhone1() {
+        return Phone1;
+    }
+
+    public void setPhone1(String Phone1) {
+        this.Phone1 = Phone1;
+    }
+
+    public String getStreetAddress2() {
+        return StreetAddress2;
+    }
+
+    public void setStreetAddress2(String StreetAddress2) {
+        this.StreetAddress2 = StreetAddress2;
+    }
+
+    public String getUnitNumber2() {
+        return UnitNumber2;
+    }
+
+    public void setUnitNumber2(String UnitNumber2) {
+        this.UnitNumber2 = UnitNumber2;
+    }
+
+    public String getCity2() {
+        return City2;
+    }
+
+    public void setCity2(String City2) {
+        this.City2 = City2;
+    }
+
+    public String getState2() {
+        return State2;
+    }
+
+    public void setState2(String State2) {
+        this.State2 = State2;
+    }
+
+    public String getZipCode2() {
+        return ZipCode2;
+    }
+
+    public void setZipCode2(String ZipCode2) {
+        this.ZipCode2 = ZipCode2;
+    }
+
+    public String getPhone2() {
+        return Phone2;
+    }
+
+    public void setPhone2(String Phone2) {
+        this.Phone2 = Phone2;
+    }
+
+    
+    
+}
